caad58f1270839955c8c5c311c8e86d8b5d8a398
maxim <uzumaxy@gmail.com>
2015-03-06 12:29:05 +0200

    Changed version of jQuery dependency.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: "uzumaxy:jstree",
   summary: "jQuery tree plugin for MeteorJS.",
-  version: "3.0.9",
+  version: "3.0.9-1",
   git: "https://github.com/uzumaxy/meteor-jstree"
 });
 
 Package.onUse(function(api) {
 
   api.use([
-    'jquery@1.11.3'
+    'jquery@1.0.1'
   ], 'client');
 
   api.addFiles([
