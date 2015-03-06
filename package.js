86b4de09621f97ac7b1f775ce2c57ebf6d2df257
maxim <uzumaxy@gmail.com>
2015-03-06 12:12:58 +0200

    Created package description.

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: "uzumaxy:jstree",
+  summary: "jQuery tree plugin for MeteorJS.",
+  version: "3.0.9",
+  git: "https://github.com/uzumaxy/meteor-jstree"
+});
+
+Package.onUse(function(api) {
+
+  api.use([
+    'jquery'
+  ], 'client');
+
+  api.addFiles([
+    'uzumaxy:jstree/js/jstree.js',
+    'uzumaxy:jstree/css/style.css'
+  ], 'client');
+
+  api.addFile([
+    'uzumaxy:jstree/img/32px.png',
+    'uzumaxy:jstree/img/40px.png',
+    'uzumaxy:jstree/img/throbber.gif'
+  ], 'client', {isAsset: true});
+
+});
