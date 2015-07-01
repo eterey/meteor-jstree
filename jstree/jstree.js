08e59549d7197fa31ebf606390f8ce7117c64663
maxim <uzumaxy@gmail.com>
2015-07-01 16:10:56 +0300

    Refactoring.

--- jstree/jstree.js
+++ jstree/jstree.js
@@ -7389,133 +7389,131 @@
   };
 
   // helpers
-  (function ($) {
-    // from http://kiro.me/projects/fuse.html
-    $.vakata.search = function (pattern, txt, options) {
-      options = options || {};
-      options = $.extend({}, $.vakata.search.defaults, options);
-      if (options.fuzzy !== false) {
-        options.fuzzy = true;
-      }
-      pattern = options.caseSensitive ? pattern : pattern.toLowerCase();
-      var MATCH_LOCATION = options.location,
-        MATCH_DISTANCE = options.distance,
-        MATCH_THRESHOLD = options.threshold,
-        patternLen = pattern.length,
-        matchmask, pattern_alphabet, match_bitapScore, search;
-      if (patternLen > 32) {
-        options.fuzzy = false;
-      }
-      if (options.fuzzy) {
-        matchmask = 1 << (patternLen - 1);
-        pattern_alphabet = (function () {
-          var mask = {},
-            i = 0;
-          for (i = 0; i < patternLen; i++) {
-            mask[pattern.charAt(i)] = 0;
-          }
-          for (i = 0; i < patternLen; i++) {
-            mask[pattern.charAt(i)] |= 1 << (patternLen - i - 1);
-          }
-          return mask;
-        }());
-        match_bitapScore = function (e, x) {
-          var accuracy = e / patternLen,
-            proximity = Math.abs(MATCH_LOCATION - x);
-          if (!MATCH_DISTANCE) {
-            return proximity ? 1.0 : accuracy;
-          }
-          return accuracy + (proximity / MATCH_DISTANCE);
+  // from http://kiro.me/projects/fuse.html
+  $.vakata.search = function (pattern, txt, options) {
+    options = options || {};
+    options = $.extend({}, $.vakata.search.defaults, options);
+    if (options.fuzzy !== false) {
+      options.fuzzy = true;
+    }
+    pattern = options.caseSensitive ? pattern : pattern.toLowerCase();
+    var MATCH_LOCATION = options.location,
+      MATCH_DISTANCE = options.distance,
+      MATCH_THRESHOLD = options.threshold,
+      patternLen = pattern.length,
+      matchmask, pattern_alphabet, match_bitapScore, search;
+    if (patternLen > 32) {
+      options.fuzzy = false;
+    }
+    if (options.fuzzy) {
+      matchmask = 1 << (patternLen - 1);
+      pattern_alphabet = (function () {
+        var mask = {},
+          i = 0;
+        for (i = 0; i < patternLen; i++) {
+          mask[pattern.charAt(i)] = 0;
+        }
+        for (i = 0; i < patternLen; i++) {
+          mask[pattern.charAt(i)] |= 1 << (patternLen - i - 1);
+        }
+        return mask;
+      }());
+      match_bitapScore = function (e, x) {
+        var accuracy = e / patternLen,
+          proximity = Math.abs(MATCH_LOCATION - x);
+        if (!MATCH_DISTANCE) {
+          return proximity ? 1.0 : accuracy;
+        }
+        return accuracy + (proximity / MATCH_DISTANCE);
+      };
+    }
+    search = function (text) {
+      text = options.caseSensitive ? text : text.toLowerCase();
+      if (pattern === text || text.indexOf(pattern) !== -1) {
+        return {
+          isMatch: true,
+          score: 0
         };
       }
-      search = function (text) {
-        text = options.caseSensitive ? text : text.toLowerCase();
-        if (pattern === text || text.indexOf(pattern) !== -1) {
-          return {
-            isMatch: true,
-            score: 0
-          };
-        }
-        if (!options.fuzzy) {
-          return {
-            isMatch: false,
-            score: 1
-          };
-        }
-        var i, j,
-          textLen = text.length,
-          scoreThreshold = MATCH_THRESHOLD,
-          bestLoc = text.indexOf(pattern, MATCH_LOCATION),
-          binMin, binMid,
-          binMax = patternLen + textLen,
-          lastRd, start, finish, rd, charMatch,
-          score = 1,
-          locations = [];
+      if (!options.fuzzy) {
+        return {
+          isMatch: false,
+          score: 1
+        };
+      }
+      var i, j,
+        textLen = text.length,
+        scoreThreshold = MATCH_THRESHOLD,
+        bestLoc = text.indexOf(pattern, MATCH_LOCATION),
+        binMin, binMid,
+        binMax = patternLen + textLen,
+        lastRd, start, finish, rd, charMatch,
+        score = 1,
+        locations = [];
+      if (bestLoc !== -1) {
+        scoreThreshold = Math.min(match_bitapScore(0, bestLoc), scoreThreshold);
+        bestLoc = text.lastIndexOf(pattern, MATCH_LOCATION + patternLen);
         if (bestLoc !== -1) {
           scoreThreshold = Math.min(match_bitapScore(0, bestLoc), scoreThreshold);
-          bestLoc = text.lastIndexOf(pattern, MATCH_LOCATION + patternLen);
-          if (bestLoc !== -1) {
-            scoreThreshold = Math.min(match_bitapScore(0, bestLoc), scoreThreshold);
-          }
         }
-        bestLoc = -1;
-        for (i = 0; i < patternLen; i++) {
-          binMin = 0;
-          binMid = binMax;
-          while (binMin < binMid) {
-            if (match_bitapScore(i, MATCH_LOCATION + binMid) <= scoreThreshold) {
-              binMin = binMid;
-            } else {
-              binMax = binMid;
-            }
-            binMid = Math.floor((binMax - binMin) / 2 + binMin);
+      }
+      bestLoc = -1;
+      for (i = 0; i < patternLen; i++) {
+        binMin = 0;
+        binMid = binMax;
+        while (binMin < binMid) {
+          if (match_bitapScore(i, MATCH_LOCATION + binMid) <= scoreThreshold) {
+            binMin = binMid;
+          } else {
+            binMax = binMid;
           }
-          binMax = binMid;
-          start = Math.max(1, MATCH_LOCATION - binMid + 1);
-          finish = Math.min(MATCH_LOCATION + binMid, textLen) + patternLen;
-          rd = new Array(finish + 2);
-          rd[finish + 1] = (1 << i) - 1;
-          for (j = finish; j >= start; j--) {
-            charMatch = pattern_alphabet[text.charAt(j - 1)];
-            if (i === 0) {
-              rd[j] = ((rd[j + 1] << 1) | 1) & charMatch;
-            } else {
-              rd[j] = ((rd[j + 1] << 1) | 1) & charMatch | (((lastRd[j + 1] | lastRd[j]) << 1) | 1) | lastRd[j + 1];
-            }
-            if (rd[j] & matchmask) {
-              score = match_bitapScore(i, j - 1);
-              if (score <= scoreThreshold) {
-                scoreThreshold = score;
-                bestLoc = j - 1;
-                locations.push(bestLoc);
-                if (bestLoc > MATCH_LOCATION) {
-                  start = Math.max(1, 2 * MATCH_LOCATION - bestLoc);
-                } else {
-                  break;
-                }
+          binMid = Math.floor((binMax - binMin) / 2 + binMin);
+        }
+        binMax = binMid;
+        start = Math.max(1, MATCH_LOCATION - binMid + 1);
+        finish = Math.min(MATCH_LOCATION + binMid, textLen) + patternLen;
+        rd = new Array(finish + 2);
+        rd[finish + 1] = (1 << i) - 1;
+        for (j = finish; j >= start; j--) {
+          charMatch = pattern_alphabet[text.charAt(j - 1)];
+          if (i === 0) {
+            rd[j] = ((rd[j + 1] << 1) | 1) & charMatch;
+          } else {
+            rd[j] = ((rd[j + 1] << 1) | 1) & charMatch | (((lastRd[j + 1] | lastRd[j]) << 1) | 1) | lastRd[j + 1];
+          }
+          if (rd[j] & matchmask) {
+            score = match_bitapScore(i, j - 1);
+            if (score <= scoreThreshold) {
+              scoreThreshold = score;
+              bestLoc = j - 1;
+              locations.push(bestLoc);
+              if (bestLoc > MATCH_LOCATION) {
+                start = Math.max(1, 2 * MATCH_LOCATION - bestLoc);
+              } else {
+                break;
               }
             }
           }
-          if (match_bitapScore(i + 1, MATCH_LOCATION) > scoreThreshold) {
-            break;
-          }
-          lastRd = rd;
         }
-        return {
-          isMatch: bestLoc >= 0,
-          score: score
-        };
+        if (match_bitapScore(i + 1, MATCH_LOCATION) > scoreThreshold) {
+          break;
+        }
+        lastRd = rd;
+      }
+      return {
+        isMatch: bestLoc >= 0,
+        score: score
       };
-      return txt === true ? {'search': search} : search(txt);
     };
-    $.vakata.search.defaults = {
-      location: 0,
-      distance: 100,
-      threshold: 0.6,
-      fuzzy: false,
-      caseSensitive: false
-    };
-  }($));
+    return txt === true ? {'search': search} : search(txt);
+  };
+  $.vakata.search.defaults = {
+    location: 0,
+    distance: 100,
+    threshold: 0.6,
+    fuzzy: false,
+    caseSensitive: false
+  };
 
   // include the search plugin by default
   // $.jstree.defaults.plugins.push("search");
@@ -7695,20 +7693,18 @@
     };
   };
 
-  (function ($, undefined) {
-    $.vakata.storage = {
-      // simply specifying the functions in FF throws an error
-      set: function (key, val) {
-        return window.localStorage.setItem(key, val);
-      },
-      get: function (key) {
-        return window.localStorage.getItem(key);
-      },
-      del: function (key) {
-        return window.localStorage.removeItem(key);
-      }
-    };
-  }($));
+  $.vakata.storage = {
+    // simply specifying the functions in FF throws an error
+    set: function (key, val) {
+      return window.localStorage.setItem(key, val);
+    },
+    get: function (key) {
+      return window.localStorage.getItem(key);
+    },
+    del: function (key) {
+      return window.localStorage.removeItem(key);
+    }
+  };
 
   // include the state plugin by default
   // $.jstree.defaults.plugins.push("state");
